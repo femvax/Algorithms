0e203a493233d7501f3a57cb2449db1c63c47f45
femvax <femvax@gmail.com>
2015-06-29 19:09:01 +0300

    More pythonic, perhaps

--- Quick sort.py
+++ Quick sort.py
@@ -1,24 +1,18 @@
+import random
 
 def sort(array):
 
     if len(array) <= 1:
         return array
 
-    pivot = len(array) // 2
-    left = []
-    right = []
-    med = []
-    for el in array:
-        if el == array[pivot]:
-            med.append(el)
-        elif el < array[pivot]:
-            left.append(el)
-        else:
-            right.append(el)
+    pivot = random.choice(array)
+
+    left = [x for x in array if x < pivot]
+    right = [x for x in array if x > pivot]
+    med = [x for x in array if x == pivot]
 
     return sort(left) + med + sort(right)
 
-import random
 test = [random.randrange(0,20,1) for x in range(20)]
 print(test)
 print(sort(test))
