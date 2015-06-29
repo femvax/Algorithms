52e039f10aceaf689e288599843024ac563bfb6d
femvax <femvax@gmail.com>
2015-06-29 13:15:23 +0300

    Add Hibbard gap

--- Shell sort.py
+++ Shell sort.py
@@ -1,6 +1,6 @@
 def sort(array):
     s_array = array
-    d_list = (5,1)
+    d_list = [2 ** i - 1 for i in range((len(s_array) // 2), 0, -1)]  # numbers from Hibbard gap, wiki for details
     for d in d_list:
         for i in range(0, len(s_array) // d):
             for j in range(i*d, 0, -d):
@@ -9,7 +9,7 @@ def sort(array):
     return s_array
 
 import random
-test = list(range(20))
+test = list(range(40))
 random.shuffle(test)
 print(test)
 print(sort(test))
