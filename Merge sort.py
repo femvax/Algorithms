9e9f0311a0a23e849a6790a2cdab27e6c2e28c20
femvax <femvax@gmail.com>
2015-06-29 14:18:33 +0300

    Need handwritten merge

---
+++ Merge sort.py
@@ -0,0 +1,19 @@
+from heapq import merge
+
+def sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left = array[:mid]
+    right = array[mid:]
+    left = sort(left)
+    right = sort(right)
+    return list(merge(left,right))
+
+import random
+test = list(range(40))
+random.shuffle(test)
+print(test)
+print(sort(test))
+
