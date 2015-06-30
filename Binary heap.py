f8de1f9dd1d9a650c8436374a7c0af80d2d26515
femvax <femvax@gmail.com>
2015-06-30 18:35:10 +0300

    Fixed

--- Binary heap.py
+++ Binary heap.py
@@ -4,16 +4,29 @@ class BinaryHeap:
         self.heap = []
 
     def insert(self, key, value):
-        self.heap.append((key,value))
-        self.heapify(len(self.heap) - 1)
+        self.heap.append((key, value))
+        self.check_up(len(self.heap) - 1)
 
-    def heapify(self, i):
+    def check_up(self, i):
         if i > 0:
            if self.heap[i][0] < self.heap[i // 2][0]:
                self.heap[i], self.heap[i // 2] = self.heap[i // 2], self.heap[i]
-           self.heapify(i // 2)
+           self.check_up(i // 2)
+
+    def check_down(self, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+        if left < len(self.heap) and self.heap[left][0] < self.heap[largest][0]:
+            largest = left
+        if right < len(self.heap) and self.heap[right][0] < self.heap[largest][0]:
+            largest = right
+        if largest != i:
+            self.heap[largest], self.heap[i] = self.heap[i], self. heap[largest]
+            self.check_down(largest)
 
     def extract(self):
-        ext = self.heap.pop(0)[1]
-        self.heapify(len(self.heap) - 1)
+        ext = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self.check_down(0)
         return ext
